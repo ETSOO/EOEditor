75aa5112ec969c27be5ba34aac227aadbc6c3f77
Garry Xiao <xz@etsoo.com>
2022-05-11 14:43:22 +1200

    Improve webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,39 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: './src/EOEditor.ts',
     devtool: 'inline-source-map',
     module: {
         rules: [
             {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
+                test: /\.(js|jsx|tsx|ts)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                { modules: false, targets: 'defaults' }
+                            ],
+                            '@babel/preset-typescript'
+                        ],
+                        plugins: [
+                            '@babel/proposal-class-properties',
+                            '@babel/proposal-object-rest-spread'
+                        ]
+                    }
+                }
             }
         ]
     },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()]
+    },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js']
+        extensions: ['.tsx', '.ts', '.js', '.jsx']
     },
     output: {
         filename: 'eoeditor.js',
