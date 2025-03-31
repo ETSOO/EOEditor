a067da4deffb02df40a7ad5c31ded18ce322c4a2
Garry Xiao <xz@etsoo.com>
2025-04-01 12:25:55 +1300

    Improve the release

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import json from '@rollup/plugin-json';
-import dts from 'rollup-plugin-dts'
 import pkg from "./package.json" with { type: "json" };
 
-const config = {
+const external = [...Object.keys(pkg.dependencies || {})];
+
+const configMjs = {
   input: "src/index.ts",
   output: [{
     dir: "lib/mjs",
@@ -12,21 +13,34 @@ const config = {
     preserveModulesRoot: "src",
     format: "es",
     exports: "named"
-  },{
+  }],
+  external: external,
+  plugins: [
+    typescript({declaration: true, declarationDir: "lib/mjs"}),
+    postcss({
+      extensions: [".css"]
+    }),
+    json()
+  ]
+};
+
+const configCjs = {
+  input: "src/index.ts",
+  output: [{
     dir: "lib/cjs",
     preserveModules: true,
     preserveModulesRoot: "src",
     format: "cjs",
     exports: "named"
   }],
-  external: [...Object.keys(pkg.dependencies || {})],
+  external: external,
   plugins: [
     typescript(),
     postcss({
       extensions: [".css"]
     }),
-    json(),
-    dts()
+    json()
   ]
 };
-export default config;
+
+export default [configMjs, configCjs];
